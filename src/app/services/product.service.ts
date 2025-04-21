e5fed7a21557ed8fe91ba16f19a7df3dac3a27b9
HARSHIT KUMAR <harshitkumar@HARSHITs-Laptop.local>
2025-04-21 11:47:39 +0530

    Added code for dynamically search for components

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,12 +3,14 @@ import { Injectable } from '@angular/core';
 import { Product } from '../common/product';
 import { Observable } from 'rxjs/internal/Observable';
 import { map } from 'rxjs/internal/operators/map';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private baseUrl='http://localhost:9090/api/products';
+  private categoryUrl='http://localhost:9090/api/product-category';
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId:number): Observable<Product[]> {
@@ -17,6 +19,16 @@ export class ProductService {
     return this.httpClient.get<GetResponse>(newUrl).pipe(
       map(response=>response._embedded.products)
     );
+  }
+  getProductCategories():Observable<ProductCategory[]>{
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response=>response._embedded.productCategory)
+    );
+  }
+}
+interface GetResponseProductCategory{
+  _embedded:{
+    productCategory:ProductCategory[];
   }
 }
 interface GetResponse{
