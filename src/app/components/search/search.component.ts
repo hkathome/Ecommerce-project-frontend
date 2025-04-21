96cbc18772a61437465b73a826b16c0007150605
HARSHIT KUMAR <harshitkumar@HARSHITs-Laptop.local>
2025-04-21 22:13:20 +0530

    Added html code for search and doSearch Method in ts file

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router:Router) { }
+  doSearch(keyword:String){
+    console.log('value:${keyword}');
+  }
   ngOnInit(): void {
   }
 
